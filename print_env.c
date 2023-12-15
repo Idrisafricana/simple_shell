31530ffcae70d41d559a2d0162c58a4148d2ee41
idrisafricana <adeleyedml2@gmail.com>
2023-12-15 06:13:32 -0800

    removed envir.c and updated print_env.c file

--- print_env.c
+++ print_env.c
@@ -1,19 +1,23 @@
 #include "shell.h"
-#include <stdlib.h>
 
 /**
-* print_env - Prints the current environment.
-*
-* Return: void
-*/
+ * unsetEnvironmentVariable - Unsets the specified environment variable.
+ * @variable: The name of the environment variable to be unset.
+ *
+ * Description: This function unsets the specified environment variable.
+ * If unsetting the variable fails, it prints an error message to stderr.
+ */
 
-void print_env(void)
+void    unsetEnvironmentVariable(char *variable)
 {
-	char **env = environ;
+        const char      *error_message = "Failed to unset environment variable: ";
+        const char      *newline = "\n";
 
-	while (*env)
-	{
-		shell_print(*env++);
-		shell_print("\n");
-	}
+        if (unsetenv(variable) != 0)
+        {
+                /* Write the error message to stderr character by character*/
+                write(STDERR_FILENO, error_message, _strlen(error_message));
+                write(STDERR_FILENO, variable, _strlen(variable));
+                write(STDERR_FILENO, newline, _strlen(newline));
+        }
 }
