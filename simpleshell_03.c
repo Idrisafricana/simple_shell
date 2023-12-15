93696f8f3284c8409dedceb545047df824f603de
idrisafricana <adeleyedml2@gmail.com>
2023-12-15 07:31:50 -0800

    update Simpleshell

--- simpleshell_03.c
+++ simpleshell_03.c
@@ -13,7 +13,7 @@
  */
 ssize_t readUserInput(char **args, size_t *size_args)
 {
-	write(STDOUT_FILENO, "Easydris$ ", 11);
+	write(STDOUT_FILENO, "EasyDris$ ", 10);
 	return (getline(args, size_args, stdin));
 }
 
